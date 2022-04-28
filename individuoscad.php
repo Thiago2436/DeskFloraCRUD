a29e728f1d9584c11ad74f47439a350fee70e30a
Thiago Martins e Martins <54513342+Thiago2436@users.noreply.github.com>
2022-04-28 12:17:42 -0300

    Add files via upload

--- individuoscad.php
+++ individuoscad.php
@@ -19,12 +19,12 @@
                
                 echo ("<h1>Ok_ .$nomepopular._cadastrado com sucesso ! </h1><br><br>");
                 
-                $lista = mysqli_query($con,"SELECT * FROM individuos")
+                $lista = mysqli_query($conn,"SELECT * FROM individuos")
                 or die ( " invalida consulta");
 
                     if(!empty($nomepopular))
                 {
-                    mysqli_query($con, "INSERT INTO individuos (nomepopular, nomecientifico, altura, diametro) 
+                    mysqli_query($conn, "INSERT INTO individuos (nomepopular, nomecientifico, altura, diametro) 
                     VALUES ('$nomepopular', '$nomecientifico','$altura','$diametro')")
                         or die ("Não inseriu ");
                 }
@@ -34,5 +34,5 @@
                  Nome Popular : $nomepopular.<br>Nome Cientifico : $nomecientifico.<br>
                 Altura: $altura.<br>
                 Diametro (cm) : $diametro.<br></h1>";           
-                     echo('<h1><a href="individuos.html"> Cadastrar Proximo </a><br><br> <a href="Login.html"> Sair/Salvar </a></h1>');
+                     echo('<h1><a href="individuos.html"> Cadastrar Proximo </a><br><br> <a href="dashboard.php"> Sair/Salvar </a></h1>');
                 ?>
